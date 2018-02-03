a7f83f8db4bbe9b3b3b064097f3a00989e44226b
Mooyooo <molomjamts@gmail.com>
2018-02-02 18:32:40 -0600

    login,ticket order components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { TicketOrderComponent } from './ticket-order/ticket-order.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    LoginComponent,
+    TicketOrderComponent
   ],
   imports: [
     BrowserModule
