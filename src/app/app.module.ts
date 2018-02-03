6b5f1fd2e028e238801d0452fc45bd0d51b92693
Mooyooo <molomjamts@gmail.com>
2018-02-03 11:14:25 -0600

    merged version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthHttp, AuthConfig } from 'angular2-jwt';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -11,7 +10,6 @@ import { TicketOrderComponent } from './ticket-order/ticket-order.component';
 import { myRoutes } from './app.routes';
 import { LoginService } from './login/login.service';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
-import { HttpClientModule } from '@angular/common/http';
 import { AuthGuard } from './guard/auth.guard';
 import { HttpClientModule } from '@angular/common/http';
 import { SignupServiceService } from './services/signup-service.service';
