915af4ef845fd1e292efda60e7c7c72ef7d8a15e
tsoohub <tsoodol_tse@yahoo.com>
2018-02-04 13:48:54 -0500

    JWT signup

--- src/app/services/signup-service.service.ts
+++ src/app/services/signup-service.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { tokenNotExpired } from 'angular2-jwt';
+import { Subscription } from 'rxjs/Subscription';
 import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Injectable()
-export class SignupServiceService {
+export class SignupServiceService implements OnDestroy{
 
+  subscribe: Subscription;
   constructor(public http: HttpClient, private router: Router) { 
   
   }
 
   signupUser(user) {
     
-    return this.http.post('/user', user).subscribe(
-      res => {
-        console.log("Data in response: "+res); 
-        this.router.navigate(['/movie']);
-      }, 
-      error => {console.log("My error: "+error); 
-    });
+    this.subscribe = this.http.post('/user', user).subscribe(
+      function (res) {
+        res => res.json();
+        localStorage.setItem('id_token', JSON.stringify(res));
+      }
+    );
   }
 
+  ngOnDestroy() {
+    this.subscribe.unsubscribe();
+  }
 }
